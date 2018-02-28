d70759c9a6f23948122268e9ab868b06e8f5b04c
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-02-28 21:53:11 +0300

    Add task 2, class Minfinder.

--- src/main/java/javase01/t02/MinFinder.java
+++ src/main/java/javase01/t02/MinFinder.java
@@ -1,7 +1,25 @@
 package javase01.t02;
 
+import static java.lang.Math.pow;
+
 public class MinFinder {
     public static void main(String[] args) {
+        double a;
+        double e;
+        double n;
 
+        e = 0.0069;
+        n = 0;
+        do {
+            n++;
+            a = 1 / pow(n + 1, 2);
+            if (a < e) {
+                System.out.println("Элемент №" + n + " = " + a);
+                System.out.println("Номер наименьшего элемента = " + n);
+                break;
+            } else {
+                System.out.println("Элемент №" + n + " = " + a);
+            }
+        } while (true);
     }
 }
