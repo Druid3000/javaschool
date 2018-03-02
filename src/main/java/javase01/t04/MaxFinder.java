ccf093acfeb077b818c0e40ee1c91ce8b382d66f
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-03-02 22:13:08 +0300

    Add resolution of task 4, module "Fundamentals"

--- src/main/java/javase01/t04/MaxFinder.java
+++ src/main/java/javase01/t04/MaxFinder.java
@@ -3,5 +3,16 @@ package javase01.t04;
 public class MaxFinder {
     public static void main(String[] args) {
 
+        double[] a = new double[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        double max = Double.NEGATIVE_INFINITY;
+        double subsequentA = max;
+        for (int i = 0; i < a.length; i++) {
+            if (subsequentA + a[i] > max) {
+                max = subsequentA + a[i];
+            }
+            subsequentA = a[i];
+        }
+        System.out.println("max = " + max);
     }
 }
