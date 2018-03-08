1bc1ccf1568aff5fe75ff3e359117700686b6b1c
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-03-08 16:15:35 +0300

    Refactoring of class MinFinder, module "Fundamentals", task 02.

--- src/main/java/javase01/t02/MinFinder.java
+++ src/main/java/javase01/t02/MinFinder.java
@@ -3,23 +3,28 @@ package javase01.t02;
 import static java.lang.Math.pow;
 
 public class MinFinder {
-    public static void main(String[] args) {
-        double a;
-        double e;
-        double n;
 
-        e = 0.0069;
-        n = 0;
-        do {
-            n++;
-            a = 1 / pow(n + 1, 2);
-            if (a < e) {
-                System.out.println("Элемент №" + n + " = " + a);
+    private static boolean conditionM(double a, double e) {
+        return a < e;
+    }
+
+    private static double getA(int n) {
+        return 1 / pow(n + 1, 2);
+    }
+
+    public static int findMin(double e) {
+        for (int n = 1; ; n++) {
+            if (conditionM(getA(n), e)) {
+                System.out.println("Элемент №" + n + " = " + getA(n));
                 System.out.println("Номер наименьшего элемента = " + n);
-                break;
+                return n;
             } else {
-                System.out.println("Элемент №" + n + " = " + a);
+                System.out.println("Элемент №" + n + " = " + getA(n));
             }
-        } while (true);
+        }
+    }
+
+    public static void main(String[] args) {
+        findMin(0.0069);
     }
 }
