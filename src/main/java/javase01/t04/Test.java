545ea1c543651416b404ffe3a9d2ab4a07e7085b
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-03-09 19:02:41 +0300

    Refactoring of test for  class "CalculatingFunctionValues", module "Fundamentals", task 03.

--- src/main/java/javase01/t04/Test.java
+++ src/main/java/javase01/t04/Test.java
@@ -2,7 +2,7 @@ package javase01.t04;
 
 public class Test {
     public static void main(String[] args) {
-        double[] a = new double[] {0, 1, 2, 100, 4, 5, 6, 7, 8, 9, 10};
+        double[] a = new double[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         MaxFinder obj = new MaxFinder(a);
 
         System.out.println("max = " + obj.getMaximum());
