654793a5d9ea72b65243e818901039b2522820a3
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-03-19 14:08:56 +0300

    Add class "Student", task05, module "OOP".

--- src/main/java/javase02/t05/Group.java
+++ src/main/java/javase02/t05/Group.java
@@ -1,4 +1,33 @@
 package javase02.t05;
 
-public class Group {
+import java.util.HashMap;
+
+public class Group <T>{
+
+    private Subject subject;
+    private HashMap<String, T> studentsAssessments = new HashMap<String, T>();
+
+    public Group(Subject subject) {
+        this.subject = subject;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void addStudent(String name, T assessment){
+        studentsAssessments.put(name, assessment);
+    }
+
+    public void removeStudent(String name){
+        studentsAssessments.remove(name);
+    }
+
+    public T getAssessmentOfStudent(String name){
+        return studentsAssessments.get(name);
+    }
+
+    public boolean isStudentInTheGroup(String name){
+        return studentsAssessments.containsKey(name);
+    }
 }
