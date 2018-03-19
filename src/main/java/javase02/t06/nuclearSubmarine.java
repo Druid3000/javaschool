09603f338643f5b30418abf072773c056957b7b2
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-03-19 22:42:29 +0300

    Add subclass "EngineOfNuclearSubmarine" in class "nuclearSubmarine", add main patameters of class "nuclearSubmarine", task06, module "OOP".

--- src/main/java/javase02/t06/nuclearSubmarine.java
+++ src/main/java/javase02/t06/nuclearSubmarine.java
@@ -1,4 +1,33 @@
 package javase02.t06;
 
 public class nuclearSubmarine {
+
+    private double speed;
+    private double depth;
+    private double voltage;
+    private EngineOfNuclearSubmarine engine;
+
+    class EngineOfNuclearSubmarine {
+        private double coolantTemperature;
+        private double depthOfLoweringControlRods;
+        private double rotationalSpeedOfTurbines;
+
+        public EngineOfNuclearSubmarine() {
+            coolantTemperature = 0;
+            depthOfLoweringControlRods = 0;
+            rotationalSpeedOfTurbines = 0;
+        }
+
+        public void startTheEngine(){
+
+        }
+
+        public void stopTheEngine(){
+
+        }
+
+        public void setDepthOfLoweringControlRods(double depthOfLoweringControlRods) {
+            this.depthOfLoweringControlRods = depthOfLoweringControlRods;
+        }
+    }
 }
