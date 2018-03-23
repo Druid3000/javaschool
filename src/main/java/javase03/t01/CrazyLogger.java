431a41e9a7ee682e42fab4f15330077490637834
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-03-23 21:02:51 +0300

    Add class "CrazyLogger", task 01, module "Strings".

---
+++ src/main/java/javase03/t01/CrazyLogger.java
@@ -0,0 +1,31 @@
+package javase03.t01;
+
+import java.util.Calendar;
+import java.util.Formatter;
+
+public class CrazyLogger {
+    StringBuilder logger = new StringBuilder();
+
+    void addMessage(String message) {
+        Calendar calendar = Calendar.getInstance();
+        Formatter formatter = new Formatter();
+        formatter.format("%1td-%<tm-%<tY : %<tH-%<tM – %s;\n", calendar, message);
+        logger.append(formatter);
+    }
+
+    String search(String str) {
+        StringBuilder result = new StringBuilder();
+        String line;
+        int beginningOfLine = 0;
+        int endOfLine = 0;
+        do {
+            endOfLine = logger.indexOf(";", beginningOfLine) + 2;
+            line = logger.substring(beginningOfLine, endOfLine);
+            beginningOfLine = endOfLine;
+
+            if (line.contains(str))
+                result.append(line);
+        } while (endOfLine != logger.length());
+        return result.toString();
+    }
+}
---
+++ src/main/java/javase03/t01/Test.java
@@ -0,0 +1,14 @@
+package javase03.t01;
+
+public class Test {
+    public static void main(String[] args) {
+        CrazyLogger logger = new CrazyLogger();
+        logger.addMessage("message1");
+        logger.addMessage("message2");
+        logger.addMessage("message2");
+        logger.addMessage("message4");
+
+        System.out.println(logger.search("message2"));
+
+    }
+}
