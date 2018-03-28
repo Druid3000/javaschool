5ed31890b68f75b016d02d97a64e08e29fcd9fdd
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-03-28 23:10:09 +0300

    Add solution of task 01, module "I/O Streams".

---
+++ src/main/java/javase04/t01/KeyWordAnalyzer.java
@@ -0,0 +1,106 @@
+package javase04.t01;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeyWordAnalyzer {
+
+    Map<String, Integer> mapOfKeyWordsAndTheirCounts = new HashMap<>();
+
+    private StringBuilder textOfReadFile = new StringBuilder();
+
+    private String[] keyWords =
+            {"abstract", "continue", "for", "new", "switch",
+                    "assert", "default", "goto", "package", "synchronized",
+                    "boolean", "do", "if", "private", "this",
+                    "break", "double", "implements", "protected", "throw",
+                    "byte", "else", "import", "public", "throws",
+                    "case", "enum", "instanceof", "return", "transient",
+                    "catch", "extends", "int", "short", "try",
+                    "char", "final", "interface", "static", "void",
+                    "class", "finally", "long", "strictfp", "volatile",
+                    "const", "float", "native", "super", "while"};
+
+    public void readFile(String fileName) {
+        int b;
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(fileName);
+            while ((b = fileInputStream.read()) != -1) {
+                textOfReadFile.append((char) b);
+            }
+            fileInputStream.close();
+        } catch (IOException e) {
+            System.err.println("Ошибка файла: " + e);
+        } finally {
+            try {
+                if (fileInputStream != null) {
+                    fileInputStream.close();
+                }
+            } catch (IOException e) {
+                System.err.println("ошибка закрытия: " + e);
+            }
+        }
+    }
+
+    public void writeFile(String filename) {
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(filename);
+            String tmp;
+            for (String x : mapOfKeyWordsAndTheirCounts.keySet()) {
+                if (mapOfKeyWordsAndTheirCounts.get(x) != 0) {
+                    tmp = x + " " + mapOfKeyWordsAndTheirCounts.get(x) + "\n";
+                    //System.out.print(tmp);
+                    fileOutputStream.write(tmp.getBytes());
+                }
+            }
+            fileOutputStream.close();
+
+        } catch (IOException e) {
+            System.err.println("ошибка записи: " + e);
+        } finally {
+            try {
+                if (fileOutputStream != null) {
+                    fileOutputStream.close();
+                }
+            } catch (IOException e) {
+                System.err.println("ошибка закрытия потока: " + e);
+            }
+        }
+    }
+
+    private void addWordToMap(String word) {
+        if (mapOfKeyWordsAndTheirCounts.containsKey(word)) {
+            Integer count = mapOfKeyWordsAndTheirCounts.get(word);
+            mapOfKeyWordsAndTheirCounts.put(word, ++count);
+        } else {
+            mapOfKeyWordsAndTheirCounts.put(word, 1);
+        }
+    }
+
+    public void searchForMatchingKeyWords() {
+        String[] splitTextOfReadFile = textOfReadFile.toString().split("[^a-zA-Z]");
+        //String[] contentArray = text.toString().split("[\\s,.\n(,{,},(,)]+");
+        //String[] contentArray = text.toString().split("(?<=[<=|>=|==|\\\\+|\\\\*|\\\\-|<|>|/|=])|(?=[<=|>=|==|\\\\+|\\\\*|\\\\-|<|>|/|=])");
+        //StringBuilder result = new StringBuilder("");
+        // int count = 0;
+        for (String i : splitTextOfReadFile) {
+            for (String j : keyWords) {
+                if (i.equals(j)) {
+                    //result.append(i+" ");
+                    //count++;
+                    //Если что, то здесь можно произвести подсчет всех слов.
+                    addWordToMap(i);
+                }
+            }
+        }
+        //result.append("count = " + count);
+
+        //System.out.println(result);
+    }
+}
+
---
+++ src/main/java/javase04/t01/Test.java
@@ -0,0 +1,11 @@
+package javase04.t01;
+
+public class Test {
+    public static void main(String[] args) {
+        KeyWordAnalyzer keyWordAnalyzer = new KeyWordAnalyzer();
+        keyWordAnalyzer.readFile("src/main/java/javase04/t01/Test.java");
+        keyWordAnalyzer.searchForMatchingKeyWords();
+        keyWordAnalyzer.writeFile("src/main/java/javase04/t01/code.txt");
+
+    }
+}
