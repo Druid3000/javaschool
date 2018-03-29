c4ed84993462bd3905a677c812873d4fdde073df
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-03-29 14:32:04 +0300

    Add function "readFile()" of class"EncodingConverter", task 03, module "I/O Streams".

--- src/main/java/javase04/t03/EncodingConverter.java
+++ src/main/java/javase04/t03/EncodingConverter.java
@@ -1,4 +1,45 @@
 package javase04.t03;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class EncodingConverter {
+
+    private StringBuilder textOfReadFile = new StringBuilder();
+
+    public void readFile(String fileName) {
+        int b;
+        FileInputStream fileInputStream = null;
+        InputStreamReader inputStreamReader;
+        BufferedReader bufferedReader = null;
+        try {
+            fileInputStream = new FileInputStream(fileName);
+            inputStreamReader = new InputStreamReader(fileInputStream, "UTF8");
+            bufferedReader = new BufferedReader(inputStreamReader);
+            while ((b = bufferedReader.read()) != -1) {
+                textOfReadFile.append((char) b);
+            }
+            fileInputStream.close();
+        } catch (IOException e) {
+            System.err.println("Ошибка файла: " + e);
+        } finally {
+            try {
+                if (fileInputStream != null) {
+                    fileInputStream.close();
+                }
+            } catch (IOException e) {
+                System.err.println("ошибка закрытия: " + e);
+            }
+            //TODO Нужно ли поместить сюда InputStreamReader?
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
