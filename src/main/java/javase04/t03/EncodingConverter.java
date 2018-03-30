6cd63239c0b5fe07caf5c4a41e684dd7d4b5fc35
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-03-30 11:23:54 +0300

    Change try with finally-block to try with resources, task 03, module "I/O Streams".

--- src/main/java/javase04/t03/EncodingConverter.java
+++ src/main/java/javase04/t03/EncodingConverter.java
@@ -7,63 +7,26 @@ public class EncodingConverter {
     private StringBuilder textOfReadFile = new StringBuilder();
 
     public void readFile(String fileName) {
-        int b;
-        FileInputStream fileInputStream = null;
-        InputStreamReader inputStreamReader; //TODO нужен ли здесь null?
-        BufferedReader bufferedReader = null;
-        try {
-            fileInputStream = new FileInputStream(fileName);
-            inputStreamReader = new InputStreamReader(fileInputStream, "UTF8");
-            bufferedReader = new BufferedReader(inputStreamReader);
+        try (FileInputStream fileInputStream = new FileInputStream(fileName);
+             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF8");
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);) {
+            int b = 0;
             while ((b = bufferedReader.read()) != -1) {
                 textOfReadFile.append((char) b);
             }
-            fileInputStream.close();
         } catch (IOException e) {
-            System.err.println("Ошибка файла: " + e);
-        } finally {
-            try {
-                if (fileInputStream != null) {
-                    fileInputStream.close();
-                }
-            } catch (IOException e) {
-                System.err.println("ошибка закрытия: " + e);
-            }
-            //TODO Нужно ли поместить сюда InputStreamReader?
-            try {
-                if (bufferedReader != null) {
-                    bufferedReader.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            System.err.println("ошибка файла: " + e);
         }
     }
 
     public void writeFile(String filename) {
-        FileOutputStream fileOutputStream = null;
-        OutputStreamWriter outputStreamWriter = null;
-        BufferedWriter bufferedWriter = null;
-        try {
-            fileOutputStream = new FileOutputStream(filename);
-            outputStreamWriter = new OutputStreamWriter(fileOutputStream, "UTF-16");
-            bufferedWriter = new BufferedWriter(outputStreamWriter);
+        try (FileOutputStream fileOutputStream = new FileOutputStream(filename);
+             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, "UTF-16");
+             BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);) {
             bufferedWriter.append(textOfReadFile.toString());
             bufferedWriter.flush();
-
         } catch (IOException e) {
-            System.err.println("ошибка записи: " + e);
-        } finally {
-
-            //TODO Нужно ли поместить сюда InputStreamReader и BufferedWriter?
-
-            try {
-                if (fileOutputStream != null) {
-                    fileOutputStream.close();
-                }
-            } catch (IOException e) {
-                System.err.println("ошибка закрытия потока: " + e);
-            }
+            System.err.println("ошибка файла: " + e);
         }
     }
 }
