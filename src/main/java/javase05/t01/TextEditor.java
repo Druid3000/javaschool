7568d668c22464d12ba7af29c39287fff993b263
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-04-01 20:52:18 +0300

    Add methods of class TextEditor, task module "Exceptions and Errors".

--- src/main/java/javase05/t01/TextEditor.java
+++ src/main/java/javase05/t01/TextEditor.java
@@ -1,4 +1,55 @@
 package javase05.t01;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class TextEditor {
+
+
+    public static String readFile(String fileName) {
+        int b;
+        FileReader fileReader = null;
+        StringBuilder textOfReadFile = null;
+        try {
+            fileReader = new FileReader(fileName);
+            textOfReadFile = new StringBuilder();
+            while ((b = fileReader.read()) != -1) {
+                textOfReadFile.append((char)b);
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка файла: " + e);
+        } finally {
+            try {
+                if (fileReader != null) {
+                    fileReader.close();
+                }
+            } catch (IOException e) {
+                System.err.println("ошибка закрытия: " + e);
+            }
+        }
+        return textOfReadFile.toString();
+    }
+
+    public static void writeFile(String filename, String text) {
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(filename);
+            fileWriter.write(text);
+        } catch (IOException e) {
+            System.err.println("ошибка записи: " + e);
+        } finally {
+            try {
+                if (fileWriter != null) {
+                    fileWriter.close();
+                }
+            } catch (IOException e) {
+                System.err.println("ошибка закрытия потока: " + e);
+            }
+        }
+    }
+
+    public static void addTextAndWriteFile(String filename, String addedText){
+        writeFile(filename,readFile(filename)+addedText);
+    }
 }
