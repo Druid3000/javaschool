27fc228804541c3cc2dc7ad65b29c0e1fece9572
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-04-01 22:47:24 +0300

    Add methods for class "ResourceManager", task 02, module "Exceptions and Errors".

--- src/main/java/javase05/t02/ResourceManager.java
+++ src/main/java/javase05/t02/ResourceManager.java
@@ -1,4 +1,27 @@
 package javase05.t02;
 
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
 public class ResourceManager {
+    private ResourceBundle resourceBundle;
+
+    public ResourceManager(String path){
+        try {
+            resourceBundle = ResourceBundle.getBundle(path);
+        }catch (MissingResourceException e){
+            System.out.println("File does not exist");
+        }
+    }
+
+    public String getValue(String key){
+        try{
+            return resourceBundle.getString(key);
+        } catch(NullPointerException e){
+            System.out.println("File does not exist");
+        } catch(MissingResourceException e){
+            System.out.println("Key does not exist");
+        }
+        return null;
+    }
 }
--- src/main/java/javase05/t02/Test.java
+++ src/main/java/javase05/t02/Test.java
@@ -1,4 +1,8 @@
 package javase05.t02;
 
 public class Test {
+    public static void main(String[] args) {
+        ResourceManager resourceManager = new ResourceManager("prop1");
+        System.out.println(resourceManager.getValue("key1"));
+    }
 }
