35c9e626273ffe9302732294831a5d2172e0228f
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-04-01 23:53:23 +0300

    Add methods for loading properties from extended file, task 02, module "Exceptions and Errors".

--- src/main/java/javase05/t02/ResourceManager.java
+++ src/main/java/javase05/t02/ResourceManager.java
@@ -1,26 +1,53 @@
 package javase05.t02;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.MissingResourceException;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 public class ResourceManager {
     private ResourceBundle resourceBundle;
+    private Properties properties = new Properties();
 
-    public ResourceManager(String path){
+    public ResourceManager(String path) {
         try {
             resourceBundle = ResourceBundle.getBundle(path);
-        }catch (MissingResourceException e){
+        } catch (MissingResourceException e) {
             System.out.println("File does not exist");
         }
     }
 
-    public String getValue(String key){
-        try{
+    public String getValue(String key) {
+        try {
             return resourceBundle.getString(key);
-        } catch(NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("File does not exist");
-        } catch(MissingResourceException e){
+        } catch (MissingResourceException e) {
             System.out.println("Key does not exist");
+        }
+        return null;
+    }
+
+    public void loadPropertyFile(String file) {
+        try (FileReader fileReader = new FileReader(file);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            properties.load(fileReader);
+        } catch (IOException e) {
+            System.out.println("File does not exist");
+        }
+    }
+
+    public String getValueFromProperty(String key) {
+        try {
+            if (properties.getProperty(key) == null)
+                throw new IllegalArgumentException("Key does not exist");
+            return properties.getProperty(key);
+        } catch (NullPointerException e) {
+            System.out.println("Key does not exist");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
         return null;
     }
--- src/main/java/javase05/t02/Test.java
+++ src/main/java/javase05/t02/Test.java
@@ -2,7 +2,10 @@ package javase05.t02;
 
 public class Test {
     public static void main(String[] args) {
-        ResourceManager resourceManager = new ResourceManager("prop1");
+        ResourceManager resourceManager = new ResourceManager("prop");
         System.out.println(resourceManager.getValue("key1"));
+
+        resourceManager.loadPropertyFile("src/main/resources/prop.properties");
+        System.out.println(resourceManager.getValueFromProperty("key1"));
     }
 }
