34b2fd6bdc4640870c55b1084005668f3af65616
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-04-02 13:23:00 +0300

    Add method printMap of class "ResourceManager", task 02, module "Generic and Collections".

--- src/main/java/javase06/t02/ResourceManager.java
+++ src/main/java/javase06/t02/ResourceManager.java
@@ -3,17 +3,21 @@ package javase06.t02;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.MissingResourceException;
-import java.util.Properties;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class ResourceManager {
     private ResourceBundle resourceBundle;
     private Properties properties = new Properties();
 
+    Map<String, String> bundleMap = new HashMap<>();
+
     public ResourceManager(String path) {
         try {
             resourceBundle = ResourceBundle.getBundle(path);
+            Set<String> z  = resourceBundle.keySet();
+            for (String key : z) {
+                bundleMap.put(key, resourceBundle.getString(key));
+            }
         } catch (MissingResourceException e) {
             System.out.println("File does not exist");
         }
@@ -23,8 +27,16 @@ public class ResourceManager {
         try (FileReader fileReader = new FileReader(file);
              BufferedReader bufferedReader = new BufferedReader(fileReader)) {
             properties.load(bufferedReader);
+            properties.putAll(bundleMap);
         } catch (IOException e) {
             System.out.println("File does not exist");
+        }
+    }
+
+    public void printBundleMap(){
+        for (Map.Entry entry : bundleMap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + " Value: "
+                    + entry.getValue());
         }
     }
 }
--- src/main/java/javase06/t02/Test.java
+++ src/main/java/javase06/t02/Test.java
@@ -2,24 +2,8 @@ package javase06.t02;
 
 public class Test {
     public static void main(String[] args) {
-
+        ResourceManager resourceManager = new ResourceManager("prop");
+        resourceManager.loadExtendedPropertyFile("src/main/resources/prop.properties");
+        resourceManager.printBundleMap();
     }
-
-
-
-    //TODO properties.putAll();
-
-
-//        Map<String, String> map = new HashMap<>();
-//        while (bundleKeys.hasMoreElements()) {
-//            String bundleKey = bundleKeys.nextElement().toString();
-//            if (bundleKey.equals("")) throw new NullPointerException("Key does not exist");
-//            String bundleValue = bundle.getString(bundleKey);
-//            map.put(bundleKey, bundleValue);
-//        }
-//        for (Map.Entry entry : map.entrySet()) {
-//            System.out.println("Key: "+entry.getKey()+ "\nValue: "+entry.getValue());
-//        }
-
-    //todo у казачковой взял вот то, что выше.
 }
