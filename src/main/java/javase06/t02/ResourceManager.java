4344cc970960acad4032b6e9d9a8f6768bec143e
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-04-02 13:04:07 +0300

    Add fields, method fo loading property, constructor of class "ResourceManager", task 02, module "Generic and Collections".

--- src/main/java/javase06/t02/ResourceManager.java
+++ src/main/java/javase06/t02/ResourceManager.java
@@ -1,4 +1,30 @@
 package javase06.t02;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.MissingResourceException;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
 public class ResourceManager {
+    private ResourceBundle resourceBundle;
+    private Properties properties = new Properties();
+
+    public ResourceManager(String path) {
+        try {
+            resourceBundle = ResourceBundle.getBundle(path);
+        } catch (MissingResourceException e) {
+            System.out.println("File does not exist");
+        }
+    }
+
+    public void loadExtendedPropertyFile(String file) {
+        try (FileReader fileReader = new FileReader(file);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            properties.load(bufferedReader);
+        } catch (IOException e) {
+            System.out.println("File does not exist");
+        }
+    }
 }
