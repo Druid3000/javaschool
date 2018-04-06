4c37ddfd1afa38e4d5a6caff5695de0345f8e386
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-04-06 22:35:30 +0300

    Add constructor of class "ResourceManager", task 02, module "Multithreading".

--- src/main/java/javase07/t02/ResourceManager.java
+++ src/main/java/javase07/t02/ResourceManager.java
@@ -2,8 +2,16 @@ package javase07.t02;
 
 import java.util.Properties;
 
-public class ResourceManager {
+public class ResourceManager extends Thread {
     private Properties properties = new Properties();
     private String pathToFile;
     private String key;
+
+    public ResourceManager(String threadName, String pathToFile, String key) {
+        super(threadName);
+        this.pathToFile = pathToFile;
+        this.key = key;
+        this.start();
+    }
+
 }
