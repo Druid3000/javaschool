268346f17bc23ccc562165815383898d12f44e7e
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-04-06 22:39:30 +0300

    Add methods "main" of class "Test", task 02, module "Multithreading".

--- src/main/java/javase07/t02/Test.java
+++ src/main/java/javase07/t02/Test.java
@@ -1,4 +1,20 @@
 package javase07.t02;
 
 public class Test {
+    public static void main(String[] args) {
+
+        String pathToFile = "src/javase07/t02/prop.properties";
+
+        ResourceManager resourceManager1
+                = new ResourceManager("Thread 1", pathToFile, "key1");
+
+        ResourceManager resourceManager2
+                = new ResourceManager("Thread 2", pathToFile, "key2");
+        try {
+            resourceManager1.join();
+            resourceManager2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
