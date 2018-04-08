7f798d98e75843211f612bfb8c05bce06e5a6f78
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-04-08 23:19:22 +0300

    Add method "getConnection()" of class "ConnectionPool" and class "Test", task02, module "JDBC".

--- src/main/java/javase10/t02/ConnectionPool.java
+++ src/main/java/javase10/t02/ConnectionPool.java
@@ -1,6 +1,8 @@
 package javase10.t02;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 
@@ -23,5 +25,28 @@ public class ConnectionPool {
         readyToUse = new PriorityQueue<>();
         alreadyInUse = new ArrayList<>();
     }
+
+    synchronized public Connection getConnection() {
+
+        if(readyToUse.size()==0)
+        {
+            try {
+                Class.forName(DB_DRIVER);
+                Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+                alreadyInUse.add(connection);
+                System.out.println("Connection is established");
+                return connection;
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("Connection is not established");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Connection is not established");
+            }
+        }
+        Connection connection = readyToUse.poll();
+        return connection;
+    }
+
 
 }
