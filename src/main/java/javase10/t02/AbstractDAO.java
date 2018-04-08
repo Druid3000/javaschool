679d54edbb0ebe9701a1d435d4600d89dcae5c8f
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-04-08 23:36:24 +0300

    Add methods of class "AbstractDAO", task02, module "JDBC".

--- src/main/java/javase10/t02/AbstractDAO.java
+++ src/main/java/javase10/t02/AbstractDAO.java
@@ -1,6 +1,9 @@
 package javase10.t02;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public abstract class AbstractDAO {
     protected Connection connection;
@@ -12,5 +15,32 @@ public abstract class AbstractDAO {
     public abstract void addSomeInformationToLibrary();
     public abstract void findSomeInformationInLibrary();
     public abstract void deleteSomeInformationFromLibrary();
+
+    //Это метод закрывает только один connection, актуальный для данного abstractDAO
+    public void close() {
+        try{
+            connection.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    //todo я не знаю, нужно ли здесь закрывать statement,
+    //Нужно, просто вызывается каждый раз этот метод и в него помещается statement
+    protected void closeStatement(Statement statement) {
+        try {
+            if (statement != null) statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void closeResultSet(ResultSet resultSet) {
+        try {
+            if (resultSet != null) resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
