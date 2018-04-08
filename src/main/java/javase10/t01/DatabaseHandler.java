d1c5c05900d95f4ffd903bf1130a5e4fbfc33080
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-04-08 14:31:17 +0300

    Add constructor of class "DatabaseHandler", task 01, module "JDBC".

--- src/main/java/javase10/t01/DatabaseHandler.java
+++ src/main/java/javase10/t01/DatabaseHandler.java
@@ -9,4 +9,11 @@ public class DatabaseHandler {
     private final String DB_PASSWORD;
     private Connection connection = null;
     private Statement batchStatement = null;
+
+    public DatabaseHandler(String DB_DRIVER, String DB_URL, String DB_USER, String DB_PASSWORD) {
+        this.DB_DRIVER      = DB_DRIVER;
+        this.DB_URL         = DB_URL;
+        this.DB_USER        = DB_USER;
+        this.DB_PASSWORD    = DB_PASSWORD;
+    }
 }
