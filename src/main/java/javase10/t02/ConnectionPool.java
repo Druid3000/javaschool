4143ad1ed276d1a361f29c7817acaa25891cfc3f
Sergey Sarvarov <sarvarov.s@mailr.ru>
2018-04-08 23:30:42 +0300

    Add methods "putConnection()",  "closeAllConnections" and "getInfo()" of class "ConnectionPool" and class "Test", task02, module "JDBC".

--- src/main/java/javase10/t02/ConnectionPool.java
+++ src/main/java/javase10/t02/ConnectionPool.java
@@ -48,5 +48,34 @@ public class ConnectionPool {
         return connection;
     }
 
+    synchronized public void putConnection(Connection connection) {
+        if(connection != null) {
+            alreadyInUse.remove(connection);
+            readyToUse.add(connection);
+        }
+    }
+
+    //TODO нужно ли закрывать конекшны здесь или достаточно в abstractDAO?
+    synchronized public void closeAllConnections() {
+        try {
+            for(Connection x : readyToUse){
+                if (x != null) x.close();
+            }
+
+            for(Connection x : alreadyInUse){
+                if (x != null) x.close();
+            }
+            System.out.println("Connections is closed");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connections is not closed");
+        }
+    }
+
+    //TODO Сервисный метод. Удалить потом его.
+    public void getInfo(){
+        System.out.println(readyToUse);
+        System.out.println(alreadyInUse);
+    }
 
 }
